73909ef088d625527ee27ce0f537d441e10a5265
ziadx33 <hatemziad384@gmail.com>
2025-03-06 06:41:06 +0200

    fix: fixed mobile-track-menu responsive

--- src/components/(app)/components/mobile-track-menu.tsx
+++ src/components/(app)/components/mobile-track-menu.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { LikeButton } from "@/components/components/track/components/like-button";
 import { Button } from "@/components/ui/button";
 import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
@@ -25,41 +26,63 @@ export function MobileTrackMenu() {
   const artistLink = `/artist/${currentData.author?.id}?playlist=${currentData.album?.id}`;
   return (
     <DrawerContent className="h-full" forceMount>
-      <div className="flex flex-col items-center gap-6 px-5 pt-6">
-        <Link href={albumLink} className=" flex items-center gap-2">
-          <RiAlbumLine /> {currentData.album?.title}
-        </Link>
-        <Link
-          href={albumLink}
-          className="relative h-fit min-h-96 w-full overflow-hidden rounded-lg"
-        >
-          <Image
-            src={currentData.track?.imgSrc ?? ""}
-            alt={currentData.track?.title ?? ""}
-            fill
-          />
-        </Link>
-        <div className="flex w-full justify-between">
-          <div className="flex flex-col">
-            <h1 className="text-2xl font-semibold">
-              {currentData.track?.title}
-            </h1>
-            <Link href={artistLink} className="text-muted-foreground">
-              {currentData.author?.name}
+      <div className="h-full overflow-auto">
+        <div className="flex h-full flex-col">
+          <div className="flex h-full flex-col items-center gap-6 px-5 pt-6">
+            <Link href={albumLink} className=" flex items-center gap-2">
+              <RiAlbumLine /> {currentData.album?.title}
             </Link>
-          </div>
-          <div className="flex gap-2">
-            <LikeButton
-              customButton={(children, handler) => (
-                <Button onClick={handler} variant="ghost" size="icon">
-                  {children}
+            <Link
+              href={albumLink}
+              className="relative flex h-fit w-full justify-center rounded-lg"
+            >
+              <img
+                src={currentData.track?.imgSrc ?? ""}
+                alt={currentData.track?.title ?? ""}
+                className="w-[80%] sm:w-[50%]"
+              />
+            </Link>
+            <div className="flex h-fit w-full justify-between">
+              <div className="flex flex-col">
+                <h1 className="text-2xl font-semibold">
+                  {currentData.track?.title}
+                </h1>
+                <Link href={artistLink} className="text-muted-foreground">
+                  {currentData.author?.name}
+                </Link>
+              </div>
+              <div className="flex gap-2">
+                <LikeButton
+                  customButton={(children, handler) => (
+                    <Button onClick={handler} variant="ghost" size="icon">
+                      {children}
+                    </Button>
+                  )}
+                  heartClasses="text-3xl"
+                  track={currentData.track}
+                  playlist={currentData.album}
+                  heartsSize={30}
+                />
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  onClick={() => navigator.clipboard.writeText(albumLink)}
+                >
+                  <LuClipboardCopy size={20} />
                 </Button>
-              )}
-              heartClasses="text-3xl"
-              track={currentData.track}
-              playlist={currentData.album}
-              heartsSize={30}
-            />
+              </div>
+            </div>
+            <div className="mt-6 flex w-full flex-col-reverse justify-center">
+              <QueueProgressBar
+                handleQueueControls={(children) => (
+                  <div className="mt-6 flex justify-center">{children}</div>
+                )}
+                className="flex justify-center"
+                duration={currentData?.track?.duration}
+              />
+            </div>
+          </div>
+          <div className="mb-[10%] mt-auto flex justify-between gap-2 px-8 pt-4">
             <Button
               variant="ghost"
               size="icon"
@@ -67,44 +90,27 @@ export function MobileTrackMenu() {
             >
               <LuClipboardCopy size={20} />
             </Button>
+            <Drawer>
+              <DrawerTrigger asChild>
+                <Button variant="ghost" size="icon">
+                  <HiOutlineQueueList size={20} />
+                </Button>
+              </DrawerTrigger>
+              <DrawerContent className="h-full">
+                <QueueMenu />
+              </DrawerContent>
+            </Drawer>
+            <TrackMoreButton
+              track={currentData.track}
+              playlist={currentData.album}
+              trigger={
+                <Button variant="ghost" size="icon">
+                  <BsThreeDots size={20} />
+                </Button>
+              }
+            />
           </div>
         </div>
-        <div className="mt-6 flex w-full flex-col-reverse">
-          <QueueProgressBar
-            handleQueueControls={(children) => (
-              <div className="mt-6 flex justify-center">{children}</div>
-            )}
-            duration={currentData?.track?.duration}
-          />
-        </div>
-      </div>
-      <div className="mb-14 mt-auto flex justify-between gap-2 px-8">
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => navigator.clipboard.writeText(albumLink)}
-        >
-          <LuClipboardCopy size={20} />
-        </Button>
-        <Drawer>
-          <DrawerTrigger asChild>
-            <Button variant="ghost" size="icon">
-              <HiOutlineQueueList size={20} />
-            </Button>
-          </DrawerTrigger>
-          <DrawerContent className="h-full">
-            <QueueMenu />
-          </DrawerContent>
-        </Drawer>
-        <TrackMoreButton
-          track={currentData.track}
-          playlist={currentData.album}
-          trigger={
-            <Button variant="ghost" size="icon">
-              <BsThreeDots size={20} />
-            </Button>
-          }
-        />
       </div>
     </DrawerContent>
   );
