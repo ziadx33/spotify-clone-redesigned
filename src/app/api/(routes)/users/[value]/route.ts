e22f24436f3d71f14898cf46ca03ee35688a3b28
ziadx33 <hatemziad384@gmail.com>
2025-03-06 05:35:32 +0200

    fix: fixed users route caching

--- src/app/api/(routes)/users/[value]/route.ts
+++ src/app/api/(routes)/users/[value]/route.ts
@@ -1,4 +1,5 @@
 import { db } from "@/server/db";
+import { unstable_cache } from "next/cache";
 import { NextResponse, type NextRequest } from "next/server";
 
 export async function GET(
@@ -11,9 +12,16 @@ export async function GET(
   const type = (searchParams.get("type") as ValueType | null) ?? "id";
   if (!value) return NextResponse.json({ error: "missing required args" });
   try {
-    const user = await db.user.findUnique({
-      where: type === "id" ? { id: value } : { email: value },
-    });
+    const keys = [`user-${value}`];
+    const user = await unstable_cache(
+      async () => {
+        return await db.user.findUnique({
+          where: type === "id" ? { id: value } : { email: value },
+        });
+      },
+      keys,
+      { tags: keys },
+    )();
     return NextResponse.json(user);
   } catch (error) {
     return NextResponse.json({ error });
