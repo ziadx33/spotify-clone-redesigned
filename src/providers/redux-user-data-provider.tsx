77f51c669a64ab18fa3101d73a1f1d7df0d708e9
ziadx33 <hatemziad384@gmail.com>
2025-03-09 19:03:35 +0200

    fix: fixed user data fetching

--- src/providers/redux-user-data-provider.tsx
+++ src/providers/redux-user-data-provider.tsx
@@ -2,13 +2,22 @@
 
 import Loading from "@/components/ui/loading";
 import { useSession } from "@/hooks/use-session";
+import { getUser } from "@/server/queries/user";
 import { editUserData } from "@/state/slices/user";
 import { type AppDispatch } from "@/state/store";
+import { useQuery } from "@tanstack/react-query";
 import { useEffect, useRef, useState, type ReactNode } from "react";
 import { useDispatch } from "react-redux";
 
 export function ReduxUserDataProvider({ children }: { children: ReactNode }) {
   const { data: user, status } = useSession();
+  const { data: userData } = useQuery({
+    queryKey: [`user-${user?.user?.id}`],
+    queryFn: async () => {
+      return getUser({ id: user?.user?.id });
+    },
+    enabled: !!user?.user,
+  });
   const [isLoading, setIsLoading] = useState(true);
   const isDone = useRef(false);
   const dispatch = useDispatch<AppDispatch>();
@@ -18,13 +27,13 @@ export function ReduxUserDataProvider({ children }: { children: ReactNode }) {
     setIsLoading(status !== "unauthenticated");
   }, [status]);
   useEffect(() => {
-    if (!user?.user) return;
+    if (!userData) return;
     if (isDone.current) return;
-    dispatch(editUserData(user.user));
+    dispatch(editUserData(userData));
     setIsLoading(false);
     isDone.current = true;
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [user?.user]);
+  }, [userData]);
 
   return !isLoading ? children : <Loading />;
 }
