529820753049ec9793a25152659bd0ef51dad837
ziadx33 <hatemziad384@gmail.com>
2025-03-15 11:27:54 +0200

    edit: changed settings design

--- src/app/(routes)/(app)/settings/page.tsx
+++ src/app/(routes)/(app)/settings/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Settings } from "@/components/settings";
+import { Settings } from "@/components/settings/test-settings";
 import { useUserData } from "@/hooks/use-user-data";
 
 export default function SettingsPage() {
